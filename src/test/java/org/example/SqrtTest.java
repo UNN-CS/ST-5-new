cc23e8fb0fb9a71766bd7e2596184aa56c642611
Frenetz <xodyrev.fedya@mail.ru>
2024-05-05 15:12:57 +0300

    fix3

--- src/test/java/org/example/SqrtTest.java
+++ src/test/java/org/example/SqrtTest.java
@@ -4,41 +4,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SqrtTest {
     @Test
-    public void testCalc() {
-        Sqrt sqrt = new Sqrt(81.0);
-        Assertions.assertEquals(9.0, sqrt.calc(), 0.00001);
-        Sqrt sqrt1 = new Sqrt(1.0);
-        Assertions.assertEquals(1.0, sqrt1.calc(), 0.001);
+    public void testingAverage() {
+        Sqrt sqrt = new Sqrt(1.0);
+        assertEquals(2.0, sqrt.average(1.0, 3.0));
+        assertEquals(5.0, sqrt.average(4.0, 6.0));        
+        assertEquals(7.0, sqrt.average(8.0, 6.0));
+        assertEquals(10.0, sqrt.average(8.0, 12.0));
+    }
+
+    @Test
+    public void testingGood() {
+        Sqrt sqrt = new Sqrt(1.0);
+        assertTrue(sqrt.good(3.0, 9.0));
+        assertFalse(sqrt.good(2.999, 9.0));        
+        assertTrue(sqrt.good(4.0, 16.0));
+        assertFalse(sqrt.good(3.999, 16.0));
+    }
+
+    @Test
+    public void testingImprove() {
+        Sqrt sqrt = new Sqrt(1.0);
+        assertEquals(3.00, sqrt.improve(2.0, 8.0));
+        assertEquals(3.5, sqrt.improve(5.0, 10.0));
+        assertEquals(4.5, sqrt.improve(5.0, 20.0));
+        assertEquals(5.0, sqrt.improve(5.0, 25.0));                
+    }
+
+    @Test
+    public void testingIter() {
+        Sqrt sqrt = new Sqrt(1.0);
+        assertEquals(2.0, sqrt.iter(1.0, 4.0), 0.00001);
+        assertEquals(2.0, sqrt.iter(1.5, 4.0), 0.00001);
+    }
+
+    @Test
+    public void testingCalc() {
         Sqrt sqrt2 = new Sqrt(-9.0);
-        Assertions.assertThrows(Throwable.class, sqrt2::calc);
-    }
-
-    @Test
-    public void testGood() {
-        Sqrt sqrt = new Sqrt(1.0);
-        Assertions.assertTrue(sqrt.good(3.0, 9.0));
-        Assertions.assertFalse(sqrt.good(2.999, 9.0));
-    }
-
-    @Test
-    public void testAverage() {
-        Sqrt sqrt = new Sqrt(1.0);
-        Assertions.assertEquals(4.5, sqrt.average(2.0, 7.0));
-        Assertions.assertEquals(6.0, sqrt.average(4.0, 8.0));
-    }
-
-
-    @Test
-    public void testIter() {
-        Sqrt sqrt = new Sqrt(1.0);
-        Assertions.assertEquals(3.0, sqrt.iter(1.0, 9.0), 0.00001);
-        Assertions.assertEquals(3.0, sqrt.iter(2.5, 9.0), 0.00001);
-    }
-
-    @Test
-    public void testImprove() {
-        Sqrt sqrt = new Sqrt(1.0);
-        Assertions.assertEquals(3.5, sqrt.improve(3.0, 9.0));
-        Assertions.assertEquals(2.449489742783178, sqrt.improve(2.0, 6.0));
+        assertThrows(Throwable.class, sqrt2::calc);
+        Sqrt sqrt1 = new Sqrt(0.0);
+        assertEquals(0.0, sqrt1.calc(), 0.001);
+        Sqrt sqrt = new Sqrt(25.0);
+        assertEquals(5.0, sqrt.calc(), 0.00001);        
+        Sqrt sqrt = new Sqrt(36.0);
+        assertEquals(6.0, sqrt.calc(), 0.00001);
     }
 }
