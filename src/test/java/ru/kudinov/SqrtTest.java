64affaf61cbe2c6a1e82c5d310cb7c63981e1306
Kudinov Nikita <kudinov.nikita@gmail.com>
2024-05-17 12:03:30 +0300

    fix: add delta to tests

--- src/test/java/ru/kudinov/SqrtTest.java
+++ src/test/java/ru/kudinov/SqrtTest.java
@@ -4,13 +4,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SqrtTest {
+    public static double delta = 1e-5
     @Test
     public void testingAverage() {
         Sqrt sqrt = new Sqrt(1.0);
-        assertEquals(2.0, sqrt.average(1.0, 3.0));
-        assertEquals(5.0, sqrt.average(4.0, 6.0));        
-        assertEquals(7.0, sqrt.average(8.0, 6.0));
-        assertEquals(10.0, sqrt.average(8.0, 12.0));
+        assertEquals(2.0, sqrt.average(1.0, 3.0), delta);
+        assertEquals(5.0, sqrt.average(4.0, 6.0), delta);        
+        assertEquals(7.0, sqrt.average(8.0, 6.0), delta);
+        assertEquals(10.0, sqrt.average(8.0, 12.0), delta);
     }
 
     @Test
@@ -25,17 +26,17 @@ public class SqrtTest {
     @Test
     public void testingImprove() {
         Sqrt sqrt = new Sqrt(1.0);
-        assertEquals(3.00, sqrt.improve(2.0, 8.0));
-        assertEquals(3.5, sqrt.improve(5.0, 10.0));
-        assertEquals(4.5, sqrt.improve(5.0, 20.0));
-        assertEquals(5.0, sqrt.improve(5.0, 25.0));                
+        assertEquals(3.00, sqrt.improve(2.0, 8.0), delta);
+        assertEquals(3.5, sqrt.improve(5.0, 10.0), delta);
+        assertEquals(4.5, sqrt.improve(5.0, 20.0), delta);
+        assertEquals(5.0, sqrt.improve(5.0, 25.0), delta);                
     }
 
     @Test
     public void testingIter() {
         Sqrt sqrt = new Sqrt(1.0);
-        assertEquals(2.0, sqrt.iter(1.0, 4.0), 0.00001);
-        assertEquals(2.0, sqrt.iter(1.5, 4.0), 0.00001);
+        assertEquals(2.0, sqrt.iter(1.0, 4.0), delta);
+        assertEquals(2.0, sqrt.iter(1.5, 4.0), delta);
     }
 
     @Test
@@ -43,10 +44,10 @@ public class SqrtTest {
         Sqrt sqrt1 = new Sqrt(-9.0);
         assertThrows(Throwable.class, sqrt1::calc);
         Sqrt sqrt2 = new Sqrt(0.0);
-        assertEquals(0.0, sqrt2.calc(), 0.001);
+        assertEquals(0.0, sqrt2.calc(), delta);
         Sqrt sqrt3 = new Sqrt(25.0);
-        assertEquals(5.0, sqrt3.calc(), 0.00001);        
+        assertEquals(5.0, sqrt3.calc(), delta);        
         Sqrt sqrt4 = new Sqrt(36.0);
-        assertEquals(6.0, sqrt4.calc(), 0.00001);
+        assertEquals(6.0, sqrt4.calc(), delta);
     }
 }
