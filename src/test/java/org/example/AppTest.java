3b91c130440235fbb7ede5a7a10993d11ce84246
ISK7 <105171814+ISK7@users.noreply.github.com>
2024-05-27 20:14:35 +0300

    added public

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -13,7 +13,7 @@ public class AppTest
     extends TestCase
 {	
 	@org.junit.jupiter.api.Test
-    void testConstructor() {
+    public void testConstructor() {
         Assertions.assertDoesNotThrow(() -> {Sqrt s = new Sqrt(2.0);});
         Assertions.assertDoesNotThrow(() -> {Sqrt s = new Sqrt(-2.0);});
         Assertions.assertDoesNotThrow(() -> {Sqrt s = new Sqrt(123456789.0);});
@@ -21,14 +21,14 @@ public class AppTest
         Assertions.assertDoesNotThrow(() -> {Sqrt s = new Sqrt(123.432);});
     }
     @org.junit.jupiter.api.Test
-    void testAverage() {
+    public void testAverage() {
         Sqrt s = new Sqrt(2.0);
         Assertions.assertEquals(s.average(2.0, 0.0), 1.0, s.delta);
         Assertions.assertEquals(s.average(12.5, 13.0), 12.75, s.delta);
         Assertions.assertEquals(s.average(2.0, -2.0), 0.0, s.delta);
     }
     @org.junit.jupiter.api.Test
-    void testGood() {
+    public void testGood() {
         Sqrt s = new Sqrt(2.0);
         Assertions.assertTrue(s.good(2.0,4.0));
         Assertions.assertTrue(s.good(2.0,3.999999999));
@@ -36,21 +36,21 @@ public class AppTest
         Assertions.assertFalse(s.good(2.0,3.999));
     }
     @org.junit.jupiter.api.Test
-    void testImprove() {
+    public void testImprove() {
         Sqrt s = new Sqrt(2.0);
         Assertions.assertEquals(s.improve(2.0, 2.0), 1.5, s.delta);
         Assertions.assertEquals(s.improve(2.0, 4.0), 2.0, s.delta);
         Assertions.assertEquals(s.improve(2.0, 0), 1.0, s.delta);
     }
     @org.junit.jupiter.api.Test
-    void testIter() {
+    public void testIter() {
         Sqrt s = new Sqrt(2.0);
         Assertions.assertEquals(s.iter(1.0, 4.0), 2.0, s.delta);
         Assertions.assertEquals(s.iter(1.0, 2.0), 1.4142135623746899, s.delta);
         Assertions.assertEquals(s.iter(2.0, 2.0), 1.4142135623746899, s.delta);
     }
     @org.junit.jupiter.api.Test
-    void testCalc() {
+    public void testCalc() {
         Sqrt s = new Sqrt(2.0);
         Assertions.assertEquals(s.calc(), 1.4142135623746899, s.delta);
         s = new Sqrt(4.0);
