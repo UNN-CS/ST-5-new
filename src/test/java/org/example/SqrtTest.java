4ed74e7ec4e7c281ad65b336923bed191ec6a23e
Frenetz <xodyrev.fedya@mail.ru>
2024-05-29 16:23:25 +0300

    fix tests

--- src/test/java/org/example/SqrtTest.java
+++ src/test/java/org/example/SqrtTest.java
@@ -2,7 +2,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SqrtTests {
+public class SqrtTest {
 
     @Test
     public void testAverage() {
