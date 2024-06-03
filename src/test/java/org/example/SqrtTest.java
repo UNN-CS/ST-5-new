2f24f325773803493232b314d0cdf181cf879cde
FireWallGR <qwert.ting@bk.ru>
2024-06-03 21:18:17 +0300

    fix

--- src/test/java/org/example/SqrtTest.java
+++ src/test/java/org/example/SqrtTest.java
@@ -6,20 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SqrtTest {
 
     @Test
-    void testAverage() {
+    public void testAverage() {
         Sqrt sqrt =new Sqrt(2.0);
         assertEquals(1.5, sqrt.average(1.0, 2.0));
     }
 
     @Test
-    void testGood() {
+    public void testGood() {
         Sqrt sqrt = new Sqrt(16.0);
         assertTrue(sqrt.good(4.0, 16.0));
         assertFalse(sqrt.good(3.9, 16.0));
     }
 
     @Test
-    void testImprove() {
+    public void testImprove() {
         Sqrt sqrt = new Sqrt(9.0);
         double guess = 3.0;
         double improved = sqrt.improve(guess, 9.0);
@@ -27,14 +27,14 @@ public class SqrtTest {
     }
 
     @Test
-    void testIter() {
+    public void testIter() {
         Sqrt sqrt = new Sqrt(25.0);
         double result = sqrt.iter(5.0, 25.0);
         assertTrue(sqrt.good(result, 25.0));
     }
 
     @Test
-    void testCalc() {
+    public void testCalc() {
         Sqrt sqrt = new Sqrt(4.0);
         assertEquals(2.0, sqrt.calc(), 0.0001);
         sqrt = new Sqrt(2.0);
