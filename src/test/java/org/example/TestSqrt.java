c36a898165a2c4856f595e867a67efff47790bde
ZhatkinVyacheslav <90867068+ZhatkinVyacheslav@users.noreply.github.com>
2024-06-03 21:20:38 +0300

    add some tests

--- src/test/java/org/example/TestSqrt.java
+++ src/test/java/org/example/TestSqrt.java
@@ -31,19 +31,19 @@ public class TestSqrt {
         assertEquals(2.0, result, 0.00000001);
     }
 
-    // @Test
-    // public void testCalc() {
-    //     Sqrt sqrt = new Sqrt(4.0);
-    //     double result = sqrt.calc();
-    //     assertEquals(2.0, result, 0.00000001);
-    // }
+    @Test
+    public void testCalc() {
+        Sqrt sqrt = new Sqrt(4.0);
+        double result = sqrt.calc();
+        assertEquals(2.0, result, 0.00000001);
+    }
 
-    // @Test
-    // public void testNegativeNumber() {
-    //     Sqrt sqrt = new Sqrt(-4.0);
-    //     double result = sqrt.calc();
-    //     assertTrue(Double.isNaN(result));
-    // }
+    @Test
+    public void testNegativeNumber() {
+        Sqrt sqrt = new Sqrt(-4.0);
+        double result = sqrt.calc();
+        assertTrue(Double.isNaN(result));
+    }
 
     // @Test
     // public void testSmallNumber() {
