ef042581b92ced79d850e3ed00eddfe41c9ee992
karrin0chka <karinka25032003@gmail.com>
2024-06-03 15:10:10 +0300

    fix2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,9 +4,9 @@ public class App
 {
     public static void main(String[] args)
     {
-        double val = Double.parseDouble("2.0");
+        double val = Double.parseDouble("3.0");
         Sqrt sqrt = new Sqrt(val);
         double result = sqrt.calc();
-        System.out.println("Sqrt of " + val + " = " + result);
+        System.out.println(sqrt.calc());
     }
 }
