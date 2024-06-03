f03f6587ff80a43cf8bc5bd8c31a5596ddcb7985
FireWallGR <qwert.ting@bk.ru>
2024-06-03 21:20:58 +0300

    remake

--- src/test/java/org/example/SqrtTest.java
+++ src/test/java/org/example/SqrtTest.java
@@ -6,9 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SqrtTest {
 
     @Test
-    public void testAverage() {
-        Sqrt sqrt =new Sqrt(2.0);
-        assertEquals(1.5, sqrt.average(1.0, 2.0));
+    public void testImprove() {
+        Sqrt sqrt = new Sqrt(9.0);
+        double guess = 3.0;
+        double improved = sqrt.improve(guess, 9.0);
+        assertEquals(3.0, improved, 0.001);
     }
 
     @Test
@@ -19,18 +21,9 @@ public class SqrtTest {
     }
 
     @Test
-    public void testImprove() {
-        Sqrt sqrt = new Sqrt(9.0);
-        double guess = 3.0;
-        double improved = sqrt.improve(guess, 9.0);
-        assertEquals(3.0, improved, 0.001);
-    }
-
-    @Test
-    public void testIter() {
-        Sqrt sqrt = new Sqrt(25.0);
-        double result = sqrt.iter(5.0, 25.0);
-        assertTrue(sqrt.good(result, 25.0));
+    public void testAverage() {
+        Sqrt sqrt = new Sqrt(2.0);
+        assertEquals(1.5, sqrt.average(1.0, 2.0));
     }
 
     @Test
@@ -39,5 +32,12 @@ public class SqrtTest {
         assertEquals(2.0, sqrt.calc(), 0.0001);
         sqrt = new Sqrt(2.0);
         assertEquals(Math.sqrt(2.0), sqrt.calc(), 0.0001);
+    }
+
+    @Test
+    public void testIter() {
+        Sqrt sqrt = new Sqrt(25.0);
+        double result = sqrt.iter(5.0, 25.0);
+        assertTrue(sqrt.good(result, 25.0));
     }
 }
