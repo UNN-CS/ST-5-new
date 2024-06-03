760df279eec17e1deafcd1b5534a2ce000c9b0c8
ZhatkinVyacheslav <90867068+ZhatkinVyacheslav@users.noreply.github.com>
2024-06-03 21:35:22 +0300

    fix4

--- src/test/java/org/example/TestSqrt.java
+++ src/test/java/org/example/TestSqrt.java
@@ -3,6 +3,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestSqrt {
+    @Test
+    public void testAverage() {
+        Sqrt sqrt = new Sqrt(0);
+        double result = sqrt.average(4.0, 2.0);
+        assertEquals(3.0, result, 0.00000001);
+    }
+
+    @Test
+    public void testGood() {
+        Sqrt sqrt = new Sqrt(0);
+        boolean result = sqrt.good(1.41421356237, 2.0);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testImprove() {
+        Sqrt sqrt = new Sqrt(0);
+        double result = sqrt.improve(1.0, 2.0);
+        assertEquals(1.5, result, 0.00000001);
+    }
+
+    @Test
+    public void testIter() {
+        Sqrt sqrt = new Sqrt(4.0);
+        double result = sqrt.iter(1.0, 4.0);
+        assertEquals(2.0, result, 0.00000001);
+    }
 
     @Test
     public void testPositiveNumber() {
@@ -32,45 +59,4 @@ public class TestSqrt {
         assertEquals(0.00001, result, 0.00000001);
     }
 
-    @Test
-    public void testLargeNumber() {
-        Sqrt sqrt = new Sqrt(1.0e15);
-        double result = sqrt.calc();
-        assertEquals(1.0e7, result, 1.0e6); // Ожидаем ошибку в 1 порядке
-    }
-
-    @Test
-    public void testVerySmallNumber() {
-        Sqrt sqrt = new Sqrt(1.0e-15);
-        double result = sqrt.calc();
-        assertEquals(1.0e-7, result, 1.0e-8); // Ожидаем ошибку в 1 порядке
-    }
-
-    @Test
-    public void testPerfectSquare() {
-        Sqrt sqrt = new Sqrt(25.0);
-        double result = sqrt.calc();
-        assertEquals(5.0, result, 0.00000001);
-    }
-
-    @Test
-    public void testNonPerfectSquare() {
-        Sqrt sqrt = new Sqrt(2.0);
-        double result = sqrt.calc();
-        assertEquals(1.41421356237, result, 0.00000001);
-    }
-
-    @Test
-    public void testLargeNonPerfectSquare() {
-        Sqrt sqrt = new Sqrt(123456789.0);
-        double result = sqrt.calc();
-        assertEquals(11111.1110605556, result, 0.00000001);
-    }
-
-    @Test
-    public void testSmallNonPerfectSquare() {
-        Sqrt sqrt = new Sqrt(0.000000000000001);
-        double result = sqrt.calc();
-        assertEquals(0.0000000316227766, result, 0.00000001);
-    }
 }
