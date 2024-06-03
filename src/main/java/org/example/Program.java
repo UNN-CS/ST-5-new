16074a8d8c1765c4da1d168b0a22dcac95f4147d
FireWallGR <qwert.ting@bk.ru>
2024-06-03 20:58:10 +0300

    fix

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -5,7 +5,7 @@ class Program
     public static void main(String[] args)
     {
         double val=Double.parseDouble("2.0");
-        Sqrt sqrt = new Sqrt(val);
+        Sqrt sqrt =new Sqrt(val);
         double result = sqrt.calc();
         System.out.println("Sqrt of " + val + " = " + result);
     }
