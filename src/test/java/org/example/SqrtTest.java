6af336f30136171002d7469e51be66cd13967413
FireWallGR <qwert.ting@bk.ru>
2024-06-03 21:16:54 +0300

    fix

--- src/test/java/org/example/SqrtTest.java
+++ src/test/java/org/example/SqrtTest.java
@@ -3,7 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class SqrtTest {
+public class SqrtTest {
 
     @Test
     void testAverage() {
