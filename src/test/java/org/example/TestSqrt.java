bda54f89adb612a117abf18f13c6e26c9ffeed41
ZhatkinVyacheslav <90867068+ZhatkinVyacheslav@users.noreply.github.com>
2024-06-03 21:54:57 +0300

    last

--- src/test/java/org/example/TestSqrt.java
+++ src/test/java/org/example/TestSqrt.java
@@ -6,32 +6,30 @@ public class TestSqrt {
 
     @Test
     public void testGoodWithSmallDelta() {
-        Sqrt sqrt6 = new Sqrt(0);
-        sqrt6.delta = 0.0001;
-        boolean result = sqrt6.good(1.41421356237, 2.0);
+        Sqrt sqrt1 = new Sqrt(0);
+        sqrt1.delta = 0.0001;
+        boolean result = sqrt1.good(1.41421356237, 2.0);
         assertTrue(result);
-    }
-
-    
+    } 
 
     @Test
     public void testImprove() {
-        Sqrt sqrt3 = new Sqrt(0);
-        double result = sqrt3.improve(1.0, 2.0);
+        Sqrt sqrt2 = new Sqrt(0);
+        double result = sqrt2.improve(1.0, 2.0);
         assertEquals(1.5, result, 0.00000001);
     }
 
     @Test
     public void testIter() {
-        Sqrt sqrt4 = new Sqrt(4.0);
-        double result = sqrt4.iter(1.0, 4.0);
+        Sqrt sqrt3 = new Sqrt(4.0);
+        double result = sqrt3.iter(1.0, 4.0);
         assertEquals(2.0, result, 0.00000001);
     }
 
     @Test
     public void testGood() {
-        Sqrt sqrt2 = new Sqrt(0);
-        boolean result = sqrt2.good(1.41421356237, 2.0);
+        Sqrt sqrt4 = new Sqrt(0);
+        boolean result = sqrt4.good(1.41421356237, 2.0);
         assertTrue(result);
     }
 
@@ -43,16 +41,9 @@ public class TestSqrt {
     }
 
     @Test
-    public void testImproveWithLargeNumbers() {
-        Sqrt sqrt = new Sqrt(0);
-        double result = sqrt.improve(1000000.0, 200000000000.0);
-        assertEquals(6000.0, result, 0.00000001);
-    }
-    
-    @Test
     public void testAverage() {
-        Sqrt sqrt1 = new Sqrt(0);
-        double result = sqrt1.average(4.0, 2.0);
+        Sqrt sqrt6 = new Sqrt(0);
+        double result = sqrt6.average(4.0, 2.0);
         assertEquals(3.0, result, 0.00000001);
     }
 
